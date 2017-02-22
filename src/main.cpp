7ec3c13b8e42fa91b0294cea47b44d9f688354f8
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-22 17:20:42 +0100

    implemented loop generating angles for scan service calls

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,56 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
+#include <vector>
 #include "ros/ros.h"
 #include "point_cloud_handler/GetPointCloud.h"
+//#include "treedwrapper/WrapperScan"
 
-typedef sensor_msgs::PointCloud2 PointCloud;
+typedef sensor_msgs::PointCloud2 PointCloudMessage;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef float degrees;
+typedef unsigned short int uint16;
 
-bool
-get_point_cloud (point_cloud_handler::GetPointCloud::Request &req,
-                     point_cloud_handler::GetPointCloud::Response &resp)
-{
-    PointCloud::Ptr pc (new PointCloud);
-    pc->header.frame_id = "some_tf_frame";
-    pc->height = pc->width = req.accuracy;
-    resp.point_cloud = *pc;
-    resp.exit_code = 0;
-    resp.error_message = "";
-    return true;
+void
+scan_object(uint16 accuracy) {
+
+    degrees y_rotate = 100 / accuracy;
+    degrees x_rotate = 90 / accuracy;
+    degrees y_min = 0;
+    degrees y_max = 359;
+    degrees x_min = -20;
+    degrees x_max = 90;
+    int times_to_scan_x = std::floor((x_max - x_min) / x_rotate);
+    int times_to_scan_y = std::floor((y_max - y_min) / y_rotate);
+    degrees y_angle = y_min;
+    degrees x_angle;
+    //std::vector<PointCloud> point_clouds = std::vector<PointCloud>();
+
+    for (int i = 0; i < times_to_scan_y; ++i)
+    {
+        x_angle = x_min;
+        std::cout << "y: " << y_angle << ", ";
+        for (int j = 0; j < times_to_scan_x; ++j)
+        {
+            std::cout << "x: " << x_angle << ", ";
+            x_angle = x_angle + x_rotate;
+        }
+        y_angle = y_angle + y_rotate;
+    }
+
+    return;
+
 }
+
 
 int
 main (int argc, char** argv)
 {
-    ros::init(argc, argv, "poin_cloud_handler");
-    ros::NodeHandle node_handle;
-
-    ros::ServiceServer service = node_handle.advertiseService("get_point_cloud", get_point_cloud);
-    ROS_INFO("Ready to serve point clouds");
-    ros::spin();
-
+    std::cout << std::endl << "accuracy 1:" << std::endl;
+    scan_object(1);
+    std::cout << std::endl << "accuracy 3:" << std::endl;
+    scan_object(3);
+    std::cout << std::endl << "accuracy 7:" << std::endl;
+    scan_object(7);
     return (0);
 }
