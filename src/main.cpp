db70bfd15fdba491e714e41b0e4549c597752cd5
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-23 10:44:10 +0100

    worked on calling the wrapper_scan

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,25 @@
 #include <vector>
 #include "ros/ros.h"
 #include "point_cloud_handler/GetPointCloud.h"
-//#include "treedwrapper/WrapperScan"
+#include "treedwrapper/WrapperScan.h"
 
-typedef sensor_msgs::PointCloud2 PointCloudMessage;
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef sensor_msgs::PointCloud2 pointCloudMessage;
+typedef pcl::PointCloud<pcl::PointXYZ> pointCloud;
+typedef treedwrapper::WrapperScan scanService;
+typedef point_cloud_handler::GetPointCloud getPointCloud;
 typedef float degrees;
 typedef unsigned short int uint16;
 
-void
-scan_object(uint16 accuracy) {
+struct scanData 
+{
+    pointCloudMessage point_cloud_message;
+    degrees y_angle;
+    degrees x_angle;
+};
+
+std::vector<scanData>
+generate_scans(uint16 accuracy) 
+{
 
     degrees y_rotate = 100 / accuracy;
     degrees x_rotate = 90 / accuracy;
@@ -29,33 +39,55 @@ scan_object(uint16 accuracy) {
     int times_to_scan_y = std::floor((y_max - y_min) / y_rotate);
     degrees y_angle = y_min;
     degrees x_angle;
-    //std::vector<PointCloud> point_clouds = std::vector<PointCloud>();
+    std::vector<scanData> scans;
+    ros::NodeHandle node_handle;
+    ros::ServiceClient client = node_handle.serviceClient<scanService>("wrapper_scan");
 
     for (int i = 0; i < times_to_scan_y; ++i)
     {
         x_angle = x_min;
-        std::cout << "y: " << y_angle << ", ";
         for (int j = 0; j < times_to_scan_x; ++j)
         {
-            std::cout << "x: " << x_angle << ", ";
             x_angle = x_angle + x_rotate;
+            scanService srv;
+            srv.request.y_angle = y_angle;
+            srv.request.x_angle = x_angle;
+            if (client.call(srv) && !srv.response.exit_code)
+            {
+                scanData scan_data;
+                scan_data.y_angle = y_angle;
+                scan_data.x_angle = x_angle;
+                scan_data.point_cloud_message = srv.response.point_cloud;
+                scans.push_back(scan_data);
+            } else 
+            {
+
+            }
         }
         y_angle = y_angle + y_rotate;
     }
 
-    return;
+
+    return scans;
 
 }
 
+bool 
+get_point_cloud(GetPointCloud::Request &req, GetPointCloud::Response &resp)
+{
+    
+    std::vector<Scan> scans = generate_scans(req.accuracy);
+    return true;
+}    
 
 int
 main (int argc, char** argv)
 {
-    std::cout << std::endl << "accuracy 1:" << std::endl;
-    scan_object(1);
-    std::cout << std::endl << "accuracy 3:" << std::endl;
-    scan_object(3);
-    std::cout << std::endl << "accuracy 7:" << std::endl;
-    scan_object(7);
+    
+    ros::init(argc, argv, "point_cloud_handler");
+    ros::NodeHandle node_handle;
+    ros::ServiceServer service = node_handle.advertiseService("get_point_cloud", get_point_cloud);
+    ROS_INFO("Ready to serve point clouds");
+
     return (0);
 }
